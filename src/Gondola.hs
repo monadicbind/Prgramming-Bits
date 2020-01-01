81483dc4faf7175d6ce3c37345a27543c396f39a
Sathish Mandapaka <sathishm@sahajsoft.com>
2020-01-01 20:46:53 +0530

    Modified the program to handle proper edge cases.

--- src/Gondola.hs
+++ src/Gondola.hs
@@ -2,23 +2,33 @@ module Gondola where
 
 import Data.List(groupBy)
 
+
 groupPeopleForGondolaRide :: Int -> [Int]-> [[Int]]
 groupPeopleForGondolaRide n input =
-  let (a,b) = foldl (foldHelper n) ([],[]) (partitionBasedOnCapacity n input)
+  let (a,b) = foldr (foldHelper n) ([],[]) (partitionBasedOnCapacity n input)
     in reverse (a:b)
 
 
-foldHelper :: Int -> ([Int],[[Int]]) -> Int -> ([Int], [[Int]])
-foldHelper n (acc , xxs) i | ((sum acc) + i <= n) = (i : acc , xxs)
-foldHelper n ( acc , xxs) i | ((sum acc) + i > n) = (i : [] ,(reverse acc) : xxs)
+foldHelper :: Int -> Int -> ([Int],[[Int]]) -> ([Int], [[Int]])
+foldHelper n i (acc , xxs) | sum acc + i <= n = (i : acc , xxs)
+foldHelper n i (acc , xxs) | sum acc + i > n = ([i] ,reverse acc : xxs)
 
 partitionBasedOnCapacity :: Int -> [Int] -> [Int]
-partitionBasedOnCapacity n input = concatMap (concatMapForPartitionBasedOnCapacity n []) input
+partitionBasedOnCapacity n input =  concat (rearrangeRemainder n (map (concatMapForPartitionBasedOnCapacity n []) input))
+
+rearrangeRemainder :: Int ->  [[Int]] -> [[Int]]
+rearrangeRemainder n =  foldr (rearrangeRemainderFold n) [[]]
+
+rearrangeRemainderFold :: Int -> [Int] -> [[Int]] -> [[Int]]
+rearrangeRemainderFold _ init []= [init]
+rearrangeRemainderFold n curr@(y:ys) acc@(x:xs)| abs (subtract n (sum x)) >= y = curr:acc
+rearrangeRemainderFold n (y :ys) acc = (ys ++[y]) : acc
 
 concatMapForPartitionBasedOnCapacity :: Int -> [Int] -> Int -> [Int]
 concatMapForPartitionBasedOnCapacity n acc input | input <= n = input : acc
 concatMapForPartitionBasedOnCapacity n acc input =
   let divisor = div input n
       remainder = mod input n
-   in if (remainder == 0) then ((replicate divisor n) ++ acc) else remainder : ((replicate divisor n)++ acc)
+      replc = replicate divisor n
+    in if (remainder == 0) then (replc ++ acc) else (remainder:(replc++ acc))
 
